ea9b856eeb628c6a89df4fc38d125d88a68e0908
drazen41 <drazen.svec@gmail.com>
2019-10-01 13:46:26 +0200

    Ch03

---
+++ Ch03.PaintingCircles/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch03.PaintingCircles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickCanvas(object sender, MouseButtonEventArgs e)
+        {
+            switch (e.ChangedButton)
+            {
+                case MouseButton.Left:
+                    var circle = new Ellipse { Stroke = Brushes.Black, StrokeThickness = 3, Fill = Brushes.Red, Width = 30, Height = 30 };
+                    var pos = e.GetPosition(_canvas);
+                    Canvas.SetLeft(circle, pos.X - circle.Width / 2);
+                    Canvas.SetTop(circle, pos.Y - circle.Height / 2);
+                    _canvas.Children.Add(circle);
+                    break;
+                case MouseButton.Middle:
+                    break;
+                case MouseButton.Right:
+                    var ellipse = e.Source as Ellipse;
+                    if (ellipse != null)
+                        _canvas.Children.Remove(ellipse);
+                    break;
+                    break;
+                case MouseButton.XButton1:
+                    break;
+                case MouseButton.XButton2:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
