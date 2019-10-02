458013d915c32cc47f5586f964d1caa2ec0dd18e
drazen41 <drazen.svec@gmail.com>
2019-10-02 15:21:35 +0200

    common dialogs

---
+++ Ch04.ContentControls/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04.ContentControls
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} is {1} years old", Name, Age);
+        }
+    }
+}
---
+++ Ch05.CommonDialogs/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch05.CommonDialogs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenImage(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Filter = "Image files|*.png;*.jpg;*.gif;*.bmp",
+                Title = "Select image to open",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+                    var bmp = new BitmapImage(new Uri(dlg.FileName));
+                    _img.Source = bmp;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Open Image");
+                }
+            }
+        }
+    }
+}
