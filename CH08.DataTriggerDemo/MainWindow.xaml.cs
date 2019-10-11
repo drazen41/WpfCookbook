05985113ad22540d18e6c497b4007ea08dc0a380
drazen41 <drazen.svec@gmail.com>
2019-10-11 12:07:18 +0200

    Ch08

---
+++ CH08.DataTriggerDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CH08.DataTriggerDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new List<Book> {
+new Book { BookName = "Windows Internals",
+AuthorName = "Mark Russinovich", IsFree = false },
+new Book { BookName = "AJAX Introduction",
+AuthorName = "Bhanwar Gupta", IsFree = true },
+new Book { BookName = "Essential COM",
+AuthorName = "Don Box", IsFree = false },
+new Book {
+BookName = "Blueprint for a Successful Presentation",
+AuthorName = "Biswajit Tripathy", IsFree = true }
+        };
+        }
+    }
+}
